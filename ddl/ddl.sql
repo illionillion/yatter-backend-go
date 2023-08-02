9970bbfa2e527fc5563a40f0025e5c9323398e7d
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-02 15:00:13 +0900

    statusesのddl追加

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -9,3 +9,12 @@ CREATE TABLE `account` (
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `statuses` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_id` bigint(20) NOT NULL, -- Foreign key referencing account.id
+  `content` longtext NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_statuses_account_id` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+);
